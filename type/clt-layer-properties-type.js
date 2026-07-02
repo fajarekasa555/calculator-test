a584af7536912847c66e6f8c3ee18760876a0861
fajareka1 <fajareka.haltec@gmail.com>
2026-07-02 16:55:51 +0700

    feat: define CLTLayerPropertiesType and PanelPropertiesType as output types

--- type/clt-layer-properties-type.js
+++ type/clt-layer-properties-type.js
@@ -1,5 +1,37 @@
 class CLTLayerPropertiesType {
-    constructor() {
-        
-    }
+  constructor() {
+    this.index = 0;
+    this.thickness = 0;
+    this.orientation = 0;
+    this.grade = '';
+    this.E = 0;
+    this.G = 0;
+    this.yMid = 0;
+    this.a = 0;
+    this.ownInertia = 0;
+    this.steinerArea = 0;
+    this.gamma = 1;
+    this.EI = 0;
+  }
+
+  static create({ index, thickness, orientation, grade, E, G, yMid, a, ownInertia, steinerArea, gamma, EI }) {
+    const props = new CLTLayerPropertiesType();
+    props.index = index;
+    props.thickness = thickness;
+    props.orientation = orientation;
+    props.grade = grade;
+    props.E = E;
+    props.G = G;
+    props.yMid = yMid;
+    props.a = a;
+    props.ownInertia = ownInertia;
+    props.steinerArea = steinerArea;
+    props.gamma = gamma;
+    props.EI = EI;
+    return props;
+  }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { CLTLayerPropertiesType };
 }
--- type/panel-properties-type.js
+++ type/panel-properties-type.js
@@ -1,5 +1,31 @@
 class PanelPropertiesType {
-    constructor() {
-        this.layers = [];
-    }
+  constructor() {
+    this.layers = [];
+    this.method = '';
+    this.totalLayers = 0;
+    this.totalThickness = 0;
+    this.beff = 0;
+    this.length = 0;
+    this.neutralAxis = 0;
+    this.EIeff = 0;
+    this.GAeff = null;
+  }
+
+  static create({ method, totalLayers, totalThickness, beff, length, neutralAxis, layers, EIeff, GAeff = null }) {
+    const result = new PanelPropertiesType();
+    result.method = method;
+    result.totalLayers = totalLayers;
+    result.totalThickness = totalThickness;
+    result.beff = beff;
+    result.length = length;
+    result.neutralAxis = neutralAxis;
+    result.layers = layers;
+    result.EIeff = EIeff;
+    result.GAeff = GAeff;
+    return result;
+  }
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { PanelPropertiesType };
 }
