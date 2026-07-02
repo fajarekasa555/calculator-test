3188f4cd8be92fb10d633aa3aeff9b70047c46b5
fajareka1 <fajareka.haltec@gmail.com>
2026-07-02 16:48:36 +0700

    feat: define MaterialGrade with MGP grade table from excel

--- type/material-grade-type.js
+++ type/material-grade-type.js
@@ -1,3 +1,26 @@
 class MaterialGrade {
-    
+  constructor(name, E, E90, G, G90) {
+    this.name = name;
+    this.E = E;
+    this.E90 = E90;
+    this.G = G;
+    this.G90 = G90;
+  }
+}
+
+const MATERIAL_GRADES = [
+  new MaterialGrade('MGP10', 1100, 110, 687.5, 62.5),
+  new MaterialGrade('MGP12', 1100, 110, 687.5, 62.5),
+];
+
+function getMaterialGrade(name) {
+  const grade = MATERIAL_GRADES.find((g) => g.name === name);
+  if (!grade) {
+    throw new Error(`Unknown material grade: ${name}`);
+  }
+  return grade;
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { MaterialGrade, MATERIAL_GRADES, getMaterialGrade };
 }
